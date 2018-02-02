d767ad201e5d2dfbd15c54ce37ae03dc0a7e344b
mabalekasihle@gmail.com <mabalekasihle@gmail.com>
2018-02-02 13:34:53 +0200

    Ionic Training Court COunter Application

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  firstTeamTotal: number = 0;
+  secondTeamTotal: number = 0;
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+
+  addToFirstTeamCounter(score : number){
+    this.firstTeamTotal = this.firstTeamTotal + score;
+    console.log("First Team Score is " + this.firstTeamTotal);
+  }
+
+  addToSecondTeamCounter(score : number){
+    this.secondTeamTotal = this.secondTeamTotal + score;
+    console.log("Second Team Score is " + this.secondTeamTotal);
+  }
+
+  resetCounter(){
+    this.firstTeamTotal = 0;
+    this.secondTeamTotal = 0;
   }
 
 }
